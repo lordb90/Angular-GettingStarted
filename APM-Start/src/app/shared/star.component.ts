9696e77091e9e21cd40c4121d8b0541edfdc6ede
BJ Pate <bpate@quantros.com>
2017-11-09 07:52:38 -0500

    merge updates

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -18,29 +17,6 @@ export class StarComponent implements OnChanges {
     }
 
     onClick(): void {
-        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+        this.ratingClicked.emit(`Current Item Rating Clicked: ${this.rating}`);
     }
 }
-=======
-import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
-
-@Component ({
-    selector: 'pm-star',
-    templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
-})
-
-export class StarComponent implements OnChanges {
-    @Input() rating: number;
-    starWidth: number;
-    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
-
-    ngOnChanges(): void {
-        this.starWidth = this.rating * 85 / 5;
-    }
-
-    onClick(): void {
-        this.ratingClicked.emit(`Current selected item has ${this.rating} rating`);
-    }
-}
->>>>>>> e558a11a24dda2bd4738556fb62246f3c42b8da7
